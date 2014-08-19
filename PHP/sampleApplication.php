96f6245b67a0aafa05c737bd69c8b4e431b5b506
unknown <dsmirnov@DSMIRNOV-W81.msk.bonasource.ru>
2014-08-19 15:30:59 +0400

    php sample: WaApiClient helper class

--- PHP/sampleApplication.php
+++ PHP/sampleApplication.php
@@ -11,127 +11,187 @@ See http://help.wildapricot.com/display/DOC/API+Version+2 for detailed descripti
 <hr>
 <?php
   
-    $apiKey = ''; //Insert your API key here
-    $adminLogin = ''; //insert admin's email here
-    $adminPassword = ''; //insert admin's password here
+    $waApiClient = WaApiClient::getInstance();
 
-    $allScopes = "general_info contacts finances events event_registrations account membership_levels settings";
+	// uncomment only one line of these two
+    // $waApiClient->initTokenByApiKey('put_your_apikey_here');
+    $waApiClient->initTokenByContactCredentials('admin@yourdomain123.net', 'your_password');
+  
+    $accountUrl = getAccountDetails() ['Url'];
+    $contacts = getContactsList() ['Contacts'];
 
-    // uncomment only one line of these two
-    $token = getAuthTokenByAdminCredentials($adminLogin, $adminPassword);
-    //$token = getAuthTokenByApiKey( $apiKey);
-    
-    $accountUrl = getAccountDetails()['Url'];
-    $contacts = getContactsList()['Contacts'];
-    foreach ($contacts as $contact) {
-        echo sprintf("#%d - %s", $contact['Id'], $contact['DisplayName']);        
-        echo '<br>';
+    foreach($contacts as $contact) {
+       echo '<br />';
+       echo sprintf("#%d - %s", $contact['Id'], $contact['DisplayName']);
     }
 
-    function getAccountDetails(){
-        $url = 'https://api.wildapricot.org/v2/Accounts/';
-        return makeAuthenticatedRequest($url)[0];  // usually you have access to one account
+    function getAccountDetails()
+    {
+       global $waApiClient;
+       $url = 'https://api.wildapricot.org/v2/Accounts/';
+       return $waApiClient->makeRequest($url) [0]; // usually you have access to one account
     }
-    
-    function getContactsList() {
-        global $accountUrl;  
+
+    function getContactsList()
+    {
+       global $waApiClient;
+       global $accountUrl;
+       $queryParams = array(
+          '$async' => 'false', // execute request synchronously
+          '$top' => 10, // limit result set to 10 records
+          '$filter' => 'Member eq true', // filter only members
+          '$select' => 'First name, Last name'
+       ); // select only first name and last name to reduce size of json data
+       $url = $accountUrl . '/Contacts/?' . http_build_query($queryParams);
+       return $waApiClient->makeRequest($url);
+    }
+
+    /**
+     * API helper class. You can copy whole class in your PHP application.
+     */
+    class WaApiClient
+    {
+       const AUTH_URL = 'https://oauth.wildapricot.org/auth/token';
+             
+       private $tokenScope = 'general_info contacts finances events event_registrations account membership_levels settings';
+
+       private static $_instance;
+       private $token;
        
-        $queryParams = array('$async'=>'false',      // execute request synchronously
-              '$top'=>10,                           // limit result set to 10 records
-              '$filter'=>'Member eq true',          // filter only members
-              '$select'=>'First name, Last name');  // select only first name and last name to reduce size of json data
-              
-        $url = $accountUrl . '/Contacts/?' . http_build_query($queryParams);   
-        return makeAuthenticatedRequest($url);      
-    }
-    
-    // this function makes authenticated request to API
-    // -----------------------
-    // $url is an absolute URL
-    // $verb is an optional parameter. 
-    // Use 'GET' to retrieve data,
-    //     'POST' to create new record
-    //     'PUT' to update existing record
-    //     'DELETE' to remove record
-    // $data is an optional parameter - data to sent to server. Pass this parameter with 'POST' or 'PUT' requests.
-    // ------------------------
-    // returns object decoded from response json 
-    function makeAuthenticatedRequest($url, $verb='GET', $data=null) {
-        global $token;
+       public function initTokenByContactCredentials($userName, $password, $scope = null)
+       {
+          if (!$scope) {
+             $this->tokenScope = $scope;
+          }
 
-        $crl = curl_init();
-        $headers = array();
-        $headers[] = "Authorization: Bearer " . $token;
-        $headers[] = "Content-Type: application/json";
+          $this->token = $this->getAuthTokenByAdminCredentials($userName, $password);
+          if (!$this->token) {
+             throw new Exception('Unable to get authorization token.');
+          }
+       }
 
-        curl_setopt($crl, CURLOPT_URL, $url);
-        curl_setopt($crl, CURLOPT_HTTPHEADER, $headers);
+       public function initTokenByApiKey($apiKey, $scope = null)
+       {
+          if (!$scope) {
+             $this->tokenScope = $scope;
+          }
 
-        
-        curl_setopt($crl, CURLOPT_CUSTOMREQUEST, $verb);
-        if($data)
-        {
-            $jsonData = json_encode($data);
-            curl_setopt($crl, CURLOPT_POSTFIELDS, $jsonData);
-        }
-        
-        curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);    
+          $this->token = $this->getAuthTokenByApiKey($apiKey);
+          if (!$this->token) {
+             throw new Exception('Unable to get authorization token.');
+          }
+       }
 
-        $jsonResult = curl_exec($crl);
-        curl_close($crl);
-        
-        // uncomment to debug
-        //echo $jsonResult;
-        $result = json_decode( $jsonResult , true );
-        
-        return $result;
-    }
-    
-    function getAuthTokenByAdminCredentials($login, $password)
-    {
-        global $allScopes;
-        
-        if($login == ''){
-            throw new Exception('login is empty');
-        }
-        
-        $data = sprintf("grant_type=%s&username=%s&password=%s&scope=%s", 'password', $login, $password, $allScopes);
-        $authorizationHeader = "Authorization: Basic " . base64_encode( "SamplePhpApplication:open_wa_api_client");
-        $result = getAuthToken($data, $authorizationHeader);
-        return $result['access_token'];      
-    }
-        
-    function getAuthTokenByApiKey($apiKey) {
-        global $allScopes;
-        
-        if($apiKey == ''){
-            throw new Exception('apiKey is empty');
-        }
-        
-        $data = sprintf("grant_type=%s&scope=%s", 'client_credentials', $allScopes);
-        $authorizationHeader = "Authorization: Basic " . base64_encode("APIKEY:" . $apiKey);
-        $result = getAuthToken($data, $authorizationHeader);
-        return $result['access_token'];     
-    }
-    
-    function getAuthToken($data, $authorizationHeader)
-    {
-        $crl = curl_init();
-        $headers = array();
-        $headers[] = $authorizationHeader;
-        $headers[] = "Content-Length: " . strlen($data);
-        curl_setopt($crl, CURLOPT_URL, "https://oauth.wildapricot.org/auth/token");
-        curl_setopt($crl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($crl, CURLOPT_POST, true);
-        curl_setopt($crl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
-        $result = json_decode(  curl_exec($crl), true );        
-        curl_close($crl);
-        return $result;      
+       // this function makes authenticated request to API
+       // -----------------------
+       // $url is an absolute URL
+       // $verb is an optional parameter.
+       // Use 'GET' to retrieve data,
+       //     'POST' to create new record
+       //     'PUT' to update existing record
+       //     'DELETE' to remove record
+       // $data is an optional parameter - data to sent to server. Pass this parameter with 'POST' or 'PUT' requests.
+       // ------------------------
+       // returns object decoded from response json
+
+       public function makeRequest($url, $verb = 'GET', $data = null)
+       {
+          if (!$this->token) {
+             throw new Exception('Access token is not initialized. Call initTokenByApiKey or initTokenByContactCredentials before performing requests.');
+          }
+
+          $ch = curl_init();
+          $headers = array(
+             'Authorization: Bearer ' . $this->token,
+             'Content-Type: application/json'
+          );
+          curl_setopt($ch, CURLOPT_URL, $url);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $verb);
+          if ($data) {
+             $jsonData = json_encode($data);
+             curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+          }
+
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          $jsonResult = curl_exec($ch);
+          if ($jsonResult === false) {
+             throw new Exception(curl_errno($ch) . ': ' . curl_error($ch));
+          }
+
+          // var_dump($jsonResult); // Uncomment line to debug response
+
+          curl_close($ch);
+          return json_decode($jsonResult, true);
+       }
+
+       private function getAuthTokenByAdminCredentials($login, $password)
+       {
+          if ($login == '') {
+             throw new Exception('login is empty');
+          }
+
+          $data = sprintf("grant_type=%s&username=%s&password=%s&scope=%s", 'password', $login, $password, $this->tokenScope);
+          $authorizationHeader = "Authorization: Basic " . base64_encode( "SamplePhpApplication:open_wa_api_client");
+          return $this->getAuthToken($data, $authorizationHeader);
+       }
+
+       private function getAuthTokenByApiKey($apiKey)
+       {
+          $data = sprintf("grant_type=%s&scope=%s", 'client_credentials', $this->tokenScope);
+          $authorizationHeader = "Authorization: Basic " . base64_encode("APIKEY:" . $apiKey);
+          return $this->getAuthToken($data, $authorizationHeader);
+       }
+
+       private function getAuthToken($data, $authorizationHeader)
+       {
+          $ch = curl_init();
+          $headers = array(
+             $authorizationHeader,
+             'Content-Length: ' . strlen($data)
+          );
+          curl_setopt($ch, CURLOPT_URL, WaApiClient::AUTH_URL);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          $result = json_decode(curl_exec($ch) , true);
+          if ($result === false) {
+             throw new Exception(curl_errno($ch) . ': ' . curl_error($ch));
+          }
+
+          // var_dump($result); // Uncomment line to debug response
+
+          curl_close($ch);
+          return $result['access_token'];
+       }
+
+       public static function getInstance()
+       {
+          if (!is_object(self::$_instance)) {
+             self::$_instance = new self();
+          }
+
+          return self::$_instance;
+       }
+
+       public final function __clone()
+       {
+          throw new Exception('It\'s impossible to clone singleton "' . __CLASS__ . '"!');
+       }
+
+       private function __construct()
+       {
+          if (!extension_loaded('curl')) {
+             throw new Exception('cURL library is not loaded');
+          }
+       }
+
+       public function __destruct()
+       {
+          $this->token = null;
+       }
     }
 ?>
 </body>
 </html>
-
-
-
