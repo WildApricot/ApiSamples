dcd9e26116d232761ec7356083016315f9a4d795
unknown <dsmirnov@DSMIRNOV-W81.msk.bonasource.ru>
2015-09-11 15:46:30 +0300

    update external applications help link

--- PHP/sampleApplication.php
+++ PHP/sampleApplication.php
@@ -139,7 +139,7 @@ See http://help.wildapricot.com/display/DOC/API+Version+2 for detailed descripti
 
           $data = sprintf("grant_type=%s&username=%s&password=%s&scope=%s", 'password', $login, $password, $this->tokenScope);
 
-          throw new Exception('Change clientId and clientSecret to values specific for your authorized application. For details see: http://help.wildapricot.com/display/DOC54/Authorizing+external+applications');
+          throw new Exception('Change clientId and clientSecret to values specific for your authorized application. For details see:  https://help.wildapricot.com/display/DOC/Authorizing+external+applications');
           $clientId = 'SamplePhpApplication';
           $clientSecret = 'open_wa_api_client';
           $authorizationHeader = "Authorization: Basic " . base64_encode( $clientId . ":" . $clientSecret);
