c67f54673c1a1a5b4ad7ac7eb33fee6808ee995d
DmitriySmirnov <dmitriy.smirnov@gmail.com>
2016-03-07 13:21:24 +0300

    draft

--- JavaScript/eventRegistrationsBagePrinter.js
+++ JavaScript/eventRegistrationsBagePrinter.js
@@ -1,45 +1,53 @@
 // this script requires jQuery
 
 
-function WApublicApi(clientId)
-{
-   this.clientId = clientId;
-   this.apiRequest = function (apiUrl, onSuccess, onError) {
-      return $.ajax({
-         url: apiUrl,
-         type: "GET",
-         dataType: "json",
-         cache: false,  
-         headers: { "clientId": this.clientId },
-         success: onSuccess, 
-         error: onError,
-         context: this
-     });
-   };
+function WApublicApi(clientId) {
+    this._initialized = false;
+    this.clientId = clientId;
+    this.apiRequest = function (apiUrl, onSuccess, onError) {
+        if (!this._initialized) {
+            throw "api client is not initialized yet. Please call init() before usaing api. Ex: $.when(api.init()).done(function(){  api.apiRequest(...); });";
+        }
+        return $.ajax({
+            url: apiUrl,
+            type: "GET",
+            dataType: "json",
+            cache: false,
+            headers: { "clientId": this.clientId },
+            success: onSuccess,
+            error: onError,
+            context: this
+        });
+    };
 
-   this._onInitSucceed = function (data, textStatus, jqXhr) {
-       this.accountId = data[0].Id;
-       alert(this);
-       alert(this.accountId);
-       this.apiUrls = {
-           me: function () { return '/sys/api/v2/accounts/' + this.accountId + '/contacts/me'; },
-           contacts: function () { return '/sys/api/v2/accounts/' + this.accountId + '/contacts' },
-           events: function () { return '/sys/api/v2/accounts/' + this.accountId + '/events' },
-           registrations: function () { return '/sys/api/v2/accounts/' + this.accountId + '/events' },
-           contactFields: '/sys/api/v2/accounts/' + this.accountId + '/contactFields',
-           invoices: '/sys/api/v2/accounts/' + this.accountId + '/invoices',
-           payments: '/sys/api/v2/accounts/' + this.accountId + '/payments',
-           tenders: '/sys/api/v2/accounts/' + this.accountId + '/tenders'
-       };
-   };
-   
+    this._onInitSucceed = function (data, textStatus, jqXhr) {
+        this.accountId = data[0].Id;
+        this.apiUrls = {
+            me: function () { return '/sys/api/v2/accounts/' + this.accountId + '/contacts/me'; },
+            contacts: function () { return '/sys/api/v2/accounts/' + this.accountId + '/contacts' },
+            events: function () { return '/sys/api/v2/accounts/' + this.accountId + '/events' },
+            registrations: function () { return '/sys/api/v2/accounts/' + this.accountId + '/events' },
+            contactFields: '/sys/api/v2/accounts/' + this.accountId + '/contactFields',
+            invoices: '/sys/api/v2/accounts/' + this.accountId + '/invoices',
+            payments: '/sys/api/v2/accounts/' + this.accountId + '/payments',
+            tenders: '/sys/api/v2/accounts/' + this.accountId + '/tenders'
+        };
+    };
 
     this.init = function () {
-        return this.apiRequest("/sys/api/v2/accounts", this._onInitSucceed,
-            function (jqXHR, textStatus, errorThrown) {
+        return $.ajax({
+            url: apiUrl,
+            type: "GET",
+            dataType: "json",
+            cache: false,
+            headers: { "clientId": this.clientId },
+            success: this._onInitSucceed,
+            error: function (jqXHR, textStatus, errorThrown) {
                 throw { status: textStatus, internalError: errorThrown };
-            });
-   };
+            },
+            context: this
+        });
+    };
 }
 
 function checkClientId() {
@@ -51,10 +59,9 @@ checkClientId();
 
 var api = new WApublicApi(clientId);
 $.when(api.init()).done(function () {
-    alert(api.accountId);
-    //api.apiRequest(api.apiUrls.me, function (data, textStatus, jqXhr) {
-    //    alert("Hello " + data.FirstName + " " + data.LastName + " !<br>Spirits say that your ID is '" + data.Id + "' and your email is '" + data.Email + "'.");
-    //});
+    api.apiRequest(api.apiUrls.me, function (data, textStatus, jqXhr) {
+        alert("Hello " + data.FirstName + " " + data.LastName + " !<br>Spirits say that your ID is '" + data.Id + "' and your email is '" + data.Email + "'.");
+    });
 });
 
 
