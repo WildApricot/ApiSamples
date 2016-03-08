ccb72e3c6c60a12bb888cb0e1de43969bd71e6c6
DmitriySmirnov <dmitriy.smirnov@gmail.com>
2016-03-08 15:23:44 +0300

    cleanup code, fix errors

--- JavaScript/eventRegistrationsBagePrinter.js
+++ JavaScript/eventRegistrationsBagePrinter.js
@@ -1,4 +1,4 @@
-// this script requires jQuery
+// this script requiresString jQuery
 
 
 function WApublicApi(clientId) {
@@ -6,52 +6,69 @@ function WApublicApi(clientId) {
     * WApublicApi is a thin wrapper around $.ajax, which helps to access wild apricot public api.
     * @clientId {String} is a valid client application ID. See https://help.wildapricot.com/display/DOC/Authenticating+API+access+from+a+Wild+Apricot+site+page for details.
     */
+
+    if (typeof(clientId) === "undefined" || clientId === null) {
+        throw "clientId is required to work with public api";
+    }
     this._initialized = false;
     this.clientId = clientId;
-    this.apiRequest = function (apiUrl, onSuccess, onError) {
+    this.apiRequest = function (params) {
+        /**
+        * Starts ajax request to api. returns result of $.ajax execution (request object)
+        * @params {Object} required parameter, with following fields
+        *      apiUrl {String} - url of api call. use api.apiUrls to get valid urls
+        *      data {Object} - optional parameters for POST and PUT requests
+        *      method {String} - http method, if not specified GET will be used
+        *      success {function} - callback on successfull call completion, syntax similar to jQuery ajax success callback
+        *      error {function} - callback on error call completion, syntax similar to jQuery ajax error callback
+        */
         if (!this._initialized) {
             throw "api client is not initialized yet. Please call init() before usaing api. Ex: $.when(api.init()).done(function(){  api.apiRequest(...); });";
         }
+        
         return $.ajax({
-            url: apiUrl,
-            type: "GET",
+            url: params.apiUrl,
+            type: params.method || "GET",
             dataType: "json",
             cache: false,
             headers: { "clientId": this.clientId },
-            success: onSuccess,
-            error: onError,
-            context: this
+            success: params.success || function(){},
+            error: params.error || function(){},
+            data: params.data
         });
     };
 
     this.apiUrls = {
         accountId: -1,
-        account: function () { return '/sys/api/v2/accounts/' + this.accountId },
+        baseApiUrl: '/sys/api',
+        account: function () { return this.baseApiUrl + '/v2/accounts/' + this.accountId; },
         me: function () {
             /**
             * contacts/me api call. Returns basic information on current logged in user
             */
-            return this.account() + '/contacts/me'
+            return this.account() + '/contacts/me';
         },
         contact: function (contactId) {
             /**
             * specific contact api call  (https://help.wildapricot.com/display/DOC/Contacts+API+V2+call#ContactsAPIV2call-Retrievinginformationforaspecificcontact)
+            * use this url to GET, PUT, DELETE contact
             * @contactId {Number}
             */
-            
-            return this.account() + '/contacts/' + contactId
+
+            return this.account() + '/contacts/' + contactId;
         },
-        contacts: function (simpleQuery, params) {
+        contacts: function (params) {
             /**
             * contacts api call with optional params (http://help.wildapricot.com/display/DOC/Contacts+API+V2+call)
-            * @simpleQuery {String} filter contacts by email, display name, first name, last name etc
-            * @params {Object} other params supported by contacts api: $async, $top, $skip, $filter
+            * use this url to list contacts or to POST new one
+            * @params {Object} other params supported by contacts api: simpleQuery, $async, $top, $skip, $filter
             */
-            
-            if (params == null) params = { };
-            
-            if (simpleQuery) params.simpleQuery = simpleQuery;
-            if (typeof (params.$async) == 'undefined' || params.$async == null) params.$async = false;
+
+            params = params || {};
+
+            if (typeof (params.$async) == 'undefined' || params.$async === null) {
+               params.$async = false;
+            }
 
             var result = this.account() + '/contacts';
 
@@ -62,37 +79,46 @@ function WApublicApi(clientId) {
             * specific event api call  (https://help.wildapricot.com/display/DOC/Events+API+V2+call#EventsAPIV2call-Retrievinginformationforaparticularevent)
             * @eventId {Number}
             */
-            return this.account() + '/events/' + eventId
+            return this.account() + '/events/' + eventId;
         },
-        events: function (simpleQuery, params) {
+        events: function (params) {
             /**
              * events api call with optional params (http://help.wildapricot.com/display/DOC/Events+API+V2+call)
-             * @simpleQuery {String} filter events by title, description, location etc
-             * @params {Object} other params supported by events api: $top, $skip, $filter, includeEventDetails
+             * @params {Object} other params supported by events api: simpleQuery, $top, $skip, $filter, includeEventDetails
              */
 
-            if (params == null) params = {};
-            if (simpleQuery) params.simpleQuery = simpleQuery;
-
+            params = params || {};
             var result = this.account() + '/events';
-
             return result + $.param(params);
+        },
+        registration: function (eventRegistrationId) {
+            /**
+             * particular event registration api call (https://help.wildapricot.com/display/DOC/EventRegistrations+API+V2+call#EventRegistrationsAPIV2call-Retrievingeventregistrationdetails)
+             * use this url to GET, PUT, DELETE particular registration
+             */
+            return this.account() + '/eventregistrations/' + eventRegistrationId;
         },
         registrations: function (params) {
             /**
-             * event registrationss api call with optional params (https://help.wildapricot.com/display/DOC/EventRegistrations+API+V2+call)
+             * event registrations api call with optional params (https://help.wildapricot.com/display/DOC/EventRegistrations+API+V2+call)
+             * use this url to list registrations or to POST a new one
              * @params {Object} params supported by event registrations api: contactId, eventId, $filter
              *                  at least one of these parameters should be passed
              */
-            return this.account() + '/eventregistrations' + $.param(params)
+            return this.account() + '/eventregistrations' + $.param(params);
         },
-        contactFields: function (params) { return this.account() + '/contactFields' + $.param(params) },
+        contactFields: function () {
+            /**
+             * contact fields api call (https://help.wildapricot.com/display/DOC/ContactFields+API+V2+call)
+             */
+            return this.account() + '/contactFields';
+        },
         invoice: function (invoiceId) {
             /**
              * particular invoice api call (https://help.wildapricot.com/display/DOC/Invoices+API+V2+call#InvoicesAPIV2call-Retrievinginformationforaparticularinvoice)
              * @invoiceId {Number}
              */
-            return this.account() + '/invoices' + $.param(params)
+            return this.account() + '/invoices/' + invoiceId;
         },
         invoices: function (params) {
             /**
@@ -100,7 +126,15 @@ function WApublicApi(clientId) {
              * @params {Object} params supported by invoices api: contactId, eventId
              *                  at least one of these parameters should be passed
              */
-            return this.account() + '/invoices' + $.param(params)
+            return this.account() + '/invoices' + $.param(params);
+        },
+        payment: function (paymentId) {
+            /**
+            * particular payment api call (https://help.wildapricot.com/display/DOC/Payments+API+V2+call)
+            * @params {Object} params supported by payments api: contactId, eventId
+            *                  at least one of these parameters should be passed
+            */
+            return this.account() + '/payments/' + paymentId;
         },
         payments: function (params) {
             /**
@@ -108,9 +142,14 @@ function WApublicApi(clientId) {
             * @params {Object} params supported by payments api: contactId, eventId
             *                  at least one of these parameters should be passed
             */
-            return this.account() + '/payments' + $.param(params)
+            return this.account() + '/payments' + $.param(params);
         },
-        tenders: function (params) { return this.account() + '/tenders' + $.param(params) }
+        tenders: function () {
+            /**
+            * tenders api call (https://help.wildapricot.com/display/DOC/Tenders+API+V2+call)
+            */
+            return this.account() + '/tenders';
+        }
     };
 
     this._onInitSucceed = function (data, textStatus, jqXhr) {
@@ -135,17 +174,10 @@ function WApublicApi(clientId) {
     };
 }
 
-function checkClientId() {
-    if (clientId) return;
-
-    alert("clientId variable is not declared");
-}
-checkClientId();
-
 var api = new WApublicApi(clientId);
-$.when(api.init())
- .done(function () {
-     api.apiRequest(api.apiUrls.me(), function (data, textStatus, jqXhr) {
-         alert("Hello " + data.FirstName + " " + data.LastName + " !<br>Spirits say that your ID is '" + data.Id + "' and your email is '" + data.Email + "'.");
-     });
+$.when(api.init()).done(function () {
+    api.apiRequest( {
+      apiUrl: api.apiUrls.me(),
+      success: function (data, textStatus, jqXhr) {
+         alert("Hello " + data.FirstName + " " + data.LastName + " !<br>Spirits say that your ID is '" + data.Id + "' and your email is '" + data.Email + "'."); } });
  });
